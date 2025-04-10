f40ce8607f830cfbb7e5d5818d3ddf6bb5b77299
Anowar Hosen <anowarhosen444@gmail.com>
2025-04-10 10:22:44 -0400

    animation added in the hero banner images

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,27 +1,59 @@
-import {motion} from "motion/react" ;
+import { motion } from "motion/react";
+import team1Img from "../../assets/team/team1.jpg";
+import team2Img from "../../assets/team/team2.jpg";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="flex-1 flex justify-center items-center ">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl border"
-            alt="Description of the image"
+        <div className="flex-1 fle-col justify-center items-center ">
+          <motion.img
+            src={team1Img}
+            animate={{ y: [20, 100, 20] }}
+            transition={{
+              duration: 9,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+            className="max-w-sm w-64 rounded-t-[35px] rounded-br-[35px] border-l-4 border-b-4 border-teal-600 shadow-2xl "
+            alt="Team celebrating image"
+          />
+          <motion.img
+            src={team2Img}
+            animate={{ x: [100, 150, 100] }}
+            transition={{
+              duration: 10,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+            className="max-w-sm w-64 rounded-t-[35px] rounded-br-[35px] border-l-4 border-b-4 border-amber-900 shadow-2xl "
+            alt="Team celebrating image"
           />
         </div>
 
-        <div className="flex-1">
+        <div className="flex-1 my-6 lg:my-0">
           <motion.h1
-          animate={{ x: 50 , color: ["red" , "green"]}}
-          transition={{ duration:
-            1,
-            delay: 0.5,
-            type: "spring",
-            ease: "easeInOut", repeat: 1, repeatType: "reverse"
-           }}
-          className="text-5xl font-bold">Latest Jobs For You !</motion.h1>
+            initial={{ x: -12, color: "#bb1065" }}
+            animate={{ x: 12 }}
+            transition={{
+              duration: 1,
+              delay: 0.5,
+              type: "spring",
+              ease: "easeInOut",
+              repeat: 1,
+              repeatType: "reverse",
+            }}
+            className="text-5xl mt-12 lg:mt-0 font-bold"
+          >
+            Latest{" "}
+            <motion.span
+              initial={{ color: "#3ef208" }}
+              animate={{ color: "#3ef208" }}
+            >
+              Jobs{" "}
+            </motion.span>{" "}
+            For You !
+          </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
